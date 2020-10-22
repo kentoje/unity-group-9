cd58afe01aa64bc724e001ecbb0835b87a69b396
TristanLemire <tristan.lemire.sio@gmail.com>
2020-10-22 17:49:13 +0200

    add scripts folder, prefabs and car prefabs v1

---
+++ Assets/Scripts/Fuil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fuil : MonoBehaviour
+{
+    public int Fuils = 3;
+
+    void Start() {
+
+    }
+
+    public void AddFuil() {
+        Fuils++;
+    }
+
+    public void LostFuil() {
+        Fuils--;
+
+        if (Fuils <= 0) {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+}
+
