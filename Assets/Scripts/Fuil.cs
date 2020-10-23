47fdd416d404c6b6a7f40dae4b526c5b11b49ea8
LimongiVirgil <limongi.virgil@gmail.com>
2020-10-23 10:15:02 +0200

    [Scoring] add score and life systeme

--- Assets/Scripts/Fuil.cs
+++ Assets/Scripts/Fuil.cs
@@ -7,17 +7,21 @@ using UnityEngine.SceneManagement;
 public class Fuil : MonoBehaviour
 {
     public int Fuils = 3;
+    public Text TxtFuils;
 
     void Start() {
-
+        TxtFuils.text = "Fuils: " + Fuils;
     }
 
     public void AddFuil() {
         Fuils++;
+        TxtFuils.text = "Fuils: " + Fuils;
     }
 
     public void LostFuil() {
         Fuils--;
+
+        TxtFuils.text = "Fuils: " + Fuils;
 
         if (Fuils <= 0) {
             SceneManager.LoadScene("GameOver");
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public int ScoreCount = 0;
+    public Text TxtScore;
+
+    void Start() {
+        TxtScore.text = "Score: " + ScoreCount;
+    }
+
+    void Update() {
+        InvokeRepeating("UpdateScore", 0.1f, 1.0f);
+    }
+    
+    void UpdateScore() {
+        ScoreCount++;
+        TxtScore.text = "Score: " + ScoreCount;
+    }
+}
