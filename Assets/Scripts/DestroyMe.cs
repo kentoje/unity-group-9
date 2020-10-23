58d5e69fc57c020d3521358405a6e2c80a9e08c8
TristanLemire <tristan.lemire.sio@gmail.com>
2020-10-23 10:42:16 +0200

    Add ammo bonus prefab

---
+++ Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void DoDestroy() {
+        // GameObject.Destroy(gameObject); == Destroy(gameObject);
+        Destroy(gameObject);
+
+        // note: this != gameObject
+    }
+}
